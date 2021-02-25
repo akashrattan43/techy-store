fe0f9e70c5d56071d19ee2a186d8b13fe8daeb4b
akash rattan <akashrattan43@Gmail.com>
2021-02-24 19:51:20 -0800

    mongodb added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from 'colors'
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -22,5 +27,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Sever is running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(`Sever is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
